cee2bf77b999374b94d899a94a0bcbee521bfda5
Dongick <87112589+Dongick@users.noreply.github.com>
2024-04-28 03:18:52 +0900

    파일 수정 ver2

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
 
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                        //configuration.setAllowedOrigins(Collections.singletonList("https://comission-platform.shop"));
+                        //configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedOrigins(Collections.singletonList("https://comission-platform.shop"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
