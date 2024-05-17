837569651d000246abd0284ccc00f5cce0bad942
Dongick <87112589+Dongick@users.noreply.github.com>
2024-05-17 18:47:23 +0900

    docker compose test (#11)
    * dockerfile 셋팅
    
    * test controller 생성
    
    * intellij 환경변수를 통한 application.yml 변경
    
    * workspace.xml 파일 삭제를 위한 .gitignore 생성
    
    * gradle.yml 편집
    
    * Dockerfile 추가
    
    * application.yml 파일 변경
    
    * config 추가
    
    * 파일 변경
    
    * 파일 수정 ver2
    
    * 파일 수정 ver3
    
    * 파일 수정 ver 4
    
    * 파일 수정 ver 5
    
    * 파일 수정 ver 6
    
    * application.yml 파일 dev와 prod 분리
    
    * 파일 수정 ver 7
    
    * docker-compose test

---
+++ demo/src/test/java/com/example/demo/mock/WithMockCustomUser.java
@@ -0,0 +1,16 @@
+package com.example.demo.mock;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+
+    String email() default "test@example.com";
+    String name() default "test";
+    String role() default "ROLE_USER";
+}
+
