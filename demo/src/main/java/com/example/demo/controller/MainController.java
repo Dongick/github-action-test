b01d291fda760960ac2b864da52e94498603d8f5
Dongick <87112589+Dongick@users.noreply.github.com>
2024-05-19 23:40:56 +0900

    docker log test

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.MainResponse;
 import com.example.demo.service.MainService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/test/main")
 @RequiredArgsConstructor
+@Slf4j
 public class MainController {
     private final MainService mainService;
 
     @GetMapping()
     public ResponseEntity<MainResponse> getInitialList() {
+        log.info("test log 입니다.");
         MainResponse mainResponse = mainService.getInitialMissionList();
 
         return ResponseEntity.ok(mainResponse);
