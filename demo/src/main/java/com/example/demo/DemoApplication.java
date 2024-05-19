53be89d3c0256f81513a8eb11b367c9f66a46fce
Dongick <87112589+Dongick@users.noreply.github.com>
2024-05-20 00:23:09 +0900

    test6

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,19 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DemoApplication {
-
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재시각 : " + new Date());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
